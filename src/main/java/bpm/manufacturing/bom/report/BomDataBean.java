dcd00a15879c986657bed746f39a5a096554f376
Jyotirmaya Dehury <jyotirmayad@mindfiresolutions.com>
2014-09-11 19:50:02 +0530

    The bom by order report DataBean file Added. This will be modified later based on the report data structure.

---
+++ src/main/java/bpm/manufacturing/bom/report/BomDataBean.java
@@ -0,0 +1,20 @@
+package bpm.manufacturing.bom.report;
+
+public class BomDataBean {
+	
+	private String orderId;
+	private String customerName;
+	
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+}
