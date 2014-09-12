7982d7d39e9f20fcb48f5bbfec33d248213af9d5
Jyotirmaya Dehury <jyotirmayad@mindfiresolutions.com>
2014-09-12 20:21:47 +0530

    Added the template data for BOM report.

--- src/main/java/bpm/manufacturing/bom/report/BomDataBeanList.java
+++ src/main/java/bpm/manufacturing/bom/report/BomDataBeanList.java
@@ -4,24 +4,60 @@ import java.util.ArrayList;
 
 public class BomDataBeanList {
 	public ArrayList<BomDataBean> getDataBeanList() {
+		
+		
+				
 		ArrayList<BomDataBean> dataBeanList = new ArrayList<BomDataBean>();
 
-		dataBeanList.add(produce("11111", "India"));
-		dataBeanList.add(produce("22222", "USA"));
-		dataBeanList.add(produce("33333", "India"));
-		dataBeanList.add(produce("44444", "California"));
+		dataBeanList.add(produce("1", "06/10/2014", "PORTA-PALETES 1 CONJUNTO DE ESTRUTURAS METÁLICAS DO TIPO PORTA-PALETES MEDINDO 12000 X 1000X 4000 MM COMPOSTO POR 4 MÓDULOS, SENDO: - 3 MÓDULOS MEDINDO 2700 X 1000 X 4000 MM COM 2PARES DE LONGARINAS PARA 2000 KGF E - 1 MÓDULO MEDINDO 3500 X 1000 X 4000 MM COM 2 PARES DELONGARINAS PARA 3000 KGF. CARGAS UNIFORMEMENTE DISTRIBUIDAS. PINTURA: COLUNAS DOS MONTANTESCINZA E LONGARINAS LARANJA. ATENÇÃO: VERIFICAR NO ITEM 'COR DA PINTURA' OS ACESSÓRIOS QUESERÃO GALVANIZADOS E SEM PINTURA.", 10, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 13898));
+		dataBeanList.add(produce("2", "07/10/2014", "1 CONJUNTO DE ESTRUTURAS METÁLICAS DO TIPO PORTA-PALETES MEDINDO 6040 X 1000 X 4000 MMCOMPOSTO POR 2 MÓDULOS, SENDO: - 1 MÓDULO MEDINDO 2300 X 1000 X 4000 MM COM 2 PARES DELONGARINAS PARA 2000 KGF E - 1 MÓDULO MEDINDO 3500 X 1000 X 4000 MM COM 2 PARES DE LONGARINASPARA 3000 KGF. CARGAS UNIFORMEMENTE DISTRIBUIDAS. PINTURA: COLUNAS DOS MONTANTES CINZA ELONGARINAS LARANJA. ATENÇÃO: VERIFICAR NO ITEM 'COR DA PINTURA' OS ACESSÓRIOS QUE SERÃOGALVANIZADOS E SEM PINTURA.", 15, "DIAGONAL GALV. esp1,55 mm C/ PARAF 1464MM", "S/ COR", 1223));
+		dataBeanList.add(produce("3", "08/10/2014", "1 MÓDULO DE ESTRUTURA METÁLICA DO TIPO PORTA-PALETES MEDINDO 2700 X 1000 X 4000 MM ( MEDIDATOTAL FRONTAL = 2860 MM ) COM 2 PARES DE LONGARINAS APTOS A SUPORTAR UMA CARGA UNIFORMEMENTEDISTRIBUIDA DE 2000 KGF CADA. CARGAS UNIFORMEMENTE DISTRIBUIDAS. PINTURA: COLUNAS DOSMONTANTES CINZA E LONGARINAS LARANJA. ATENÇÃO: VERIFICAR NO ITEM 'COR DA PINTURA' OSACESSÓRIOS QUE SERÃO GALVANIZADOS E SEM PINTURA.", 15, "DIAGONAL GALV. esp1,55 mm C/ PARAF 1464MM", "S/ COR", 1223));
+		dataBeanList.add(produce("4", "011/10/2014", "1 MÓDULO DE ESTRUTURA METÁLICA DO TIPO PORTA-PALETES MEDINDO 2300 X 1000 X 4000 MM ( MEDIDATOTAL FRONTAL = 2460 MM ) COM 2 PARES DE LONGARINAS APTOS A SUPORTAR UMA CARGA UNIFORMEMENTEDISTRIBUIDA DE 2000 KGF CADA. CARGAS UNIFORMEMENTE DISTRIBUIDAS. PINTURA: COLUNAS DOSMONTANTES CINZA E LONGARINAS LARANJA. ATENÇÃO: VERIFICAR NO ITEM 'COR DA PINTURA' OSACESSÓRIOS QUE SERÃO GALVANIZADOS E SEM PINTURA.", 15, "DIAGONAL GALV. esp1,55 mm C/ PARAF 1464MM", "S/ COR", 1223));
+		
+		dataBeanList.add(produce("1", "06/10/2014", "", 10, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 1211));
+		dataBeanList.add(produce("1", "06/10/2014", "", 12, "DIAGONAL GALV. esp1,55 mm C/ PARAF 1464MM", "CZ-PAD", 34));
+		dataBeanList.add(produce("1", "06/10/2014", "", 11, "LONGARINA FECHADA 150 CH14 MED3500MM", "CZ-PAD", 22));
+		dataBeanList.add(produce("1", "06/10/2014", "", 113, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 444));
+		dataBeanList.add(produce("1", "06/10/2014", "", 133, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 34));
+		
+		dataBeanList.add(produce("2", "06/10/2014", "", 10, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 1211));
+		dataBeanList.add(produce("2", "06/10/2014", "", 12, "DIAGONAL GALV. esp1,55 mm C/ PARAF 1464MM", "CZ-PAD", 34));
+		dataBeanList.add(produce("2", "06/10/2014", "", 11, "LONGARINA FECHADA 150 CH14 MED3500MM", "CZ-PAD", 22));
+		dataBeanList.add(produce("2", "06/10/2014", "", 113, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 444));
+		dataBeanList.add(produce("2", "06/10/2014", "", 133, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 34));
+		
+		dataBeanList.add(produce("3", "06/10/2014", "", 10, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 1211));
+		dataBeanList.add(produce("3", "06/10/2014", "", 12, "DIAGONAL GALV. esp1,55 mm C/ PARAF 1464MM", "CZ-PAD", 34));
+		dataBeanList.add(produce("3", "06/10/2014", "", 11, "LONGARINA FECHADA 150 CH14 MED3500MM", "CZ-PAD", 22));
+		dataBeanList.add(produce("3", "06/10/2014", "", 113, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 444));
+		dataBeanList.add(produce("3", "06/10/2014", "", 133, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 34));
+		
+		dataBeanList.add(produce("4", "06/10/2014", "", 10, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 1211));
+		dataBeanList.add(produce("4", "06/10/2014", "", 12, "DIAGONAL GALV. esp1,55 mm C/ PARAF 1464MM", "CZ-PAD", 34));
+		dataBeanList.add(produce("4", "06/10/2014", "", 11, "LONGARINA FECHADA 150 CH14 MED3500MM", "CZ-PAD", 22));
+		dataBeanList.add(produce("4", "06/10/2014", "", 113, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 444));
+		dataBeanList.add(produce("4", "06/10/2014", "", 133, "COLUNA NORMAL 1,80 4000MM", "CZ-PAD", 34));
 
+		
 		return dataBeanList;
 	}
 
+
+
 	/**
-	* This method returns a DataBean object,
-	* with name and country set in it.
+	* This method returns a BomDataBean object,
+	* 
 	*/
-	private BomDataBean produce(String orderId, String name) {
+	private BomDataBean produce(String itemNo, String deliveryDate, String description, int bomQty, String bomDescription, String bomColor, int bomWeight) {
 		BomDataBean dataBean = new BomDataBean();
-		dataBean.setOrderId(orderId);
-		dataBean.setCustomerName(name);
+		dataBean.setItemNo(itemNo);
+		dataBean.setDeliveryDate(deliveryDate);
+		dataBean.setDescription(description);
+		dataBean.setBomQty(bomQty);
+		dataBean.setBomDescription(bomDescription);
+		dataBean.setBomColor(bomColor);
+		dataBean.setBomWeight(bomWeight);
+		dataBean.setBomWeightTotal(bomQty * bomWeight);
 		return dataBean;
 	}
 }
