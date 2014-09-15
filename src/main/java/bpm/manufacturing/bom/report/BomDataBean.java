0b1c9fc6ce83ecaf9b60f05b5fe34f82aa6e00d4
jyotirmayad <jyotirmayad@mindfiresolutions.com>
2014-09-15 17:50:51 +0530

    updated the structure.

--- src/main/java/bpm/manufacturing/bom/report/BomDataBean.java
+++ src/main/java/bpm/manufacturing/bom/report/BomDataBean.java
@@ -5,62 +5,54 @@ package bpm.manufacturing.bom.report;
 public class BomDataBean {
 	
 	private String itemNo;
-	private String deliveryDate;
-	private String description;
+	private String itemDescription;
+	private String prodQty;
+	private String prodDescription;
+	private String prodColor;
+	private String prodWeight;
+	private String prodWeightTotal;
 	
-	private int bomQty;
-	private String bomDescription;
-	private String bomColor;
-	private int bomWeight;
-	private int bomWeightTotal;
-	
-	public int getBomQty() {
-		return bomQty;
-	}
-	public int getBomWeightTotal() {
-		return bomWeightTotal;
-	}
-	public void setBomWeightTotal(int bomWeightTotal) {
-		this.bomWeightTotal = bomWeightTotal;
-	}
-	public void setBomQty(int bomQty) {
-		this.bomQty = bomQty;
-	}
-	public String getBomDescription() {
-		return bomDescription;
-	}
-	public void setBomDescription(String bomDescription) {
-		this.bomDescription = bomDescription;
-	}
-	public String getBomColor() {
-		return bomColor;
-	}
-	public void setBomColor(String bomColor) {
-		this.bomColor = bomColor;
-	}
-	public int getBomWeight() {
-		return bomWeight;
-	}
-	public void setBomWeight(int bomWeight) {
-		this.bomWeight = bomWeight;
-	}
 	public String getItemNo() {
 		return itemNo;
 	}
 	public void setItemNo(String itemNo) {
 		this.itemNo = itemNo;
 	}
-	public String getDeliveryDate() {
-		return deliveryDate;
+	public String getItemDescription() {
+		return itemDescription;
 	}
-	public void setDeliveryDate(String deliveryDate) {
-		this.deliveryDate = deliveryDate;
+	public void setItemDescription(String itemDescription) {
+		this.itemDescription = itemDescription;
 	}
-	public String getDescription() {
-		return description;
+	public String getProdQty() {
+		return prodQty;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+	public void setProdQty(String quantity) {
+		this.prodQty = quantity;
+	}
+	public String getProdDescription() {
+		return prodDescription;
+	}
+	public void setProdDescription(String prodDescription) {
+		this.prodDescription = prodDescription;
+	}
+	public String getProdColor() {
+		return prodColor;
+	}
+	public void setProdColor(String prodColor) {
+		this.prodColor = prodColor;
+	}
+	public String getProdWeight() {
+		return prodWeight;
+	}
+	public void setProdWeight(String weight) {
+		this.prodWeight = weight;
+	}
+	public String getProdWeightTotal() {
+		return prodWeightTotal;
+	}
+	public void setProdWeightTotal(String weight) {
+		this.prodWeightTotal = weight;
 	}
 	
 }
