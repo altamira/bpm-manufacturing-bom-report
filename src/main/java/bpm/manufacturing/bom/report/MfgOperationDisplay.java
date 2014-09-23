91e9fec92e0fdb290273d6ab481fd8d686e3d73f
jyotirmayad <jyotirmayad@mindfiresolutions.com>
2014-09-23 20:25:29 +0530

    Added the class to have the list data structure for "Processo de Fabricação" report

---
+++ src/main/java/bpm/manufacturing/bom/report/MfgOperationDisplay.java
@@ -0,0 +1,70 @@
+package bpm.manufacturing.bom.report;
+
+public class MfgOperationDisplay {
+	private String name;
+	private String inputCodeList;
+	private String inputMaterialList;
+	private String inputQtyList;
+	private String outputCodeList;
+	private String outputMaterialList;
+	private String outputQtyList;
+	private String descriptionOperation;
+	private String scetchOfOperation;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getInputCodeList() {
+		return inputCodeList;
+	}
+	public void setInputCodeList(String inputCodeList) {
+		this.inputCodeList = inputCodeList;
+	}
+	public String getInputMaterialList() {
+		return inputMaterialList;
+	}
+	public void setInputMaterialList(String inputMaterialList) {
+		this.inputMaterialList = inputMaterialList;
+	}
+	public String getInputQtyList() {
+		return inputQtyList;
+	}
+	public void setInputQtyList(String inputQtyList) {
+		this.inputQtyList = inputQtyList;
+	}
+	public String getOutputCodeList() {
+		return outputCodeList;
+	}
+	public void setOutputCodeList(String outputCodeList) {
+		this.outputCodeList = outputCodeList;
+	}
+	public String getOutputMaterialList() {
+		return outputMaterialList;
+	}
+	public void setOutputMaterialList(String outputMaterialList) {
+		this.outputMaterialList = outputMaterialList;
+	}
+	public String getOutputQtyList() {
+		return outputQtyList;
+	}
+	public void setOutputQtyList(String outputQtyList) {
+		this.outputQtyList = outputQtyList;
+	}
+	public String getDescriptionOperation() {
+		return descriptionOperation;
+	}
+	public void setDescriptionOperation(String descriptionOperation) {
+		this.descriptionOperation = descriptionOperation;
+	}
+	public String getScetchOfOperation() {
+		return scetchOfOperation;
+	}
+	public void setScetchOfOperation(String scetchOfOperation) {
+		this.scetchOfOperation = scetchOfOperation;
+	}
+	
+
+}
