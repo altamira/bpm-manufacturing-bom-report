e373dc288a0173cc1f08e7fc867d04f4fda518be
jyotirmayad <jyotirmayad@mindfiresolutions.com>
2014-09-23 20:32:10 +0530

    Added the function to get the image logo buffer.

--- src/main/java/bpm/manufacturing/bom/report/MfgProcessReport.java
+++ src/main/java/bpm/manufacturing/bom/report/MfgProcessReport.java
@@ -1,5 +1,9 @@
 package bpm.manufacturing.bom.report;
 
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -15,6 +19,20 @@ public class MfgProcessReport {
 		WebTarget webTarget = client.target("http://localhost:8080/bpm-manufacturing-bom-report/mfg-process.json");
 		MfgProcessDataBean mfgProcessData = webTarget.path("").request(MediaType.APPLICATION_JSON).get(MfgProcessDataBean.class);
 		return mfgProcessData;
+	}
+	
+	public BufferedImage getLogo() {
+		//GET THE LOGO FILE FROM RESOURCE
+		InputStream reportLogo = getClass().getResourceAsStream("/images/report-header-logo.png");
+		
+		 BufferedImage imfg = null;
+		 try {
+		     //InputStream in = new ByteArrayInputStream(requestReportAltamiraimage);
+		     imfg = ImageIO.read(reportLogo);
+		 } catch (Exception e1) {
+		     e1.printStackTrace();
+		 }
+		 return imfg;
 	}
 
 }
