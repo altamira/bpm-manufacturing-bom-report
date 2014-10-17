56ad73c7c82ccf11f2996830306a49276ef200da
jyotirmayad <jyotirmayad@mindfiresolutions.com>
2014-10-17 18:48:46 +0530

    Updated the material list report to be driven by the java object mapping with the json object, rather than parsing the json string.

---
+++ src/main/java/bpm/manufacturing/bom/report/MaterialListReport.java
@@ -0,0 +1,138 @@
+package bpm.manufacturing.bom.report;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+public class MaterialListReport {
+	
+	
+public OrderDataBean getData(String code) {
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target("http://localhost:8081/bpm-manufacturing-bom-report/order_72201.json");
+		OrderDataBean OrderData = webTarget.path("").request(MediaType.APPLICATION_JSON).get(OrderDataBean.class);
+		return OrderData;
+	}
+
+	public Response getReport(String code) throws ServletException, IOException{
+		try {
+			JasperPrint jasperPrint;
+			byte[] pdf = null;
+			OrderDataBean reportData = this.getData(code);
+			long createdTimeStamp = (long) reportData.getCreated();
+			long deliveryTimeStamp = (long) reportData.getDelivery();
+			
+			//FORMATING ORDER DATE
+			String orderDateDisplay = new java.text.SimpleDateFormat("dd/MM/yyyy").format(new java.util.Date(createdTimeStamp));
+			
+			//FORMATING DELIVERT DATE
+			String deliveryDateDisplay = new java.text.SimpleDateFormat("w/yyyy").format(new java.util.Date(deliveryTimeStamp));
+			String weekString = new java.text.SimpleDateFormat("w").format(new java.util.Date(deliveryTimeStamp));
+			int weekNo = Integer.parseInt( weekString );
+			String yearString = new java.text.SimpleDateFormat("yyyy").format(new java.util.Date(deliveryTimeStamp));
+			int yearNo = Integer.parseInt( yearString );
+	
+			// CALCULATE THE 1ST DAY AND LAST DAY OF THE WEEK
+			Calendar calendar = Calendar.getInstance();
+			calendar.clear();
+			calendar.set(Calendar.WEEK_OF_YEAR, weekNo);
+			calendar.set(Calendar.YEAR, yearNo);
+			// Get the FIRST day of week.
+			Date date = calendar.getTime();
+			String startDateDisplay = new java.text.SimpleDateFormat("dd/MM/yyyy").format(date);
+			// Get the LAST day of week.
+			calendar.add(Calendar.DATE, 6);
+			Date endDate = calendar.getTime();
+			String endDateDisplay = new java.text.SimpleDateFormat("dd/MM/yyyy").format(endDate);
+			String deliveryWeekDisplay = startDateDisplay + " a " + endDateDisplay;
+			
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			//SET THE PARAMETERS FOR JASPER REPORT
+	        parameters.put("Title", "Lista de Material - " + code);
+	     	parameters.put("UserName", "MASTER");
+	     	parameters.put("Customer", reportData.getCustomer());
+	     	parameters.put("Representative", reportData.getRepresentative());
+	     	parameters.put("OrderID", reportData.getNumber());
+	     	parameters.put("LogoImage", this.getLogo());
+	     	parameters.put("DateRequest", orderDateDisplay);
+	     	parameters.put("DeliveryTime", deliveryDateDisplay);
+	     	parameters.put("DeliveryWeek", deliveryWeekDisplay);
+	     	parameters.put("NoBudget", reportData.getQuotation());
+	     	parameters.put("NoProject", reportData.getProject());
+	     	parameters.put("Finish", reportData.getFinish());
+	     	parameters.put("Comment", reportData.getComment());
+	     	ArrayList<OrderItemProductDataBean> dataList = new ArrayList<OrderItemProductDataBean>();
+	     	ArrayList<OrderItemDataBean> OrderItemList = reportData.getItems();
+	     	System.out.println(OrderItemList.size());
+	     	for (int i = 0; i < OrderItemList.size(); i++) {
+	     		System.out.println(i);
+				ArrayList<OrderItemProductDataBean> OrderItemProductList = OrderItemList.get(i).getParts();
+				for (int j = 0; j < OrderItemProductList.size(); j++) {
+					//System.out.println(OrderItemProductList.get(j).getQuantity());
+					OrderItemProductList.get(j).setItemCode(OrderItemList.get(i).getItem());
+					OrderItemProductList.get(j).setItemDescription(OrderItemList.get(i).getDescription());
+					dataList.add(OrderItemProductList.get(j));
+				}
+				
+	     	}
+	     	
+	     	//GET THE JASPER FILE
+			InputStream reportStream = getClass().getResourceAsStream("/reports/bom-by-order-report.jasper");
+			if (reportStream == null) {
+				return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Não foi possivel carregar o relatorio !").build();
+			}
+		
+			//PRINT THE PDF REPORT
+
+    		JRBeanCollectionDataSource beanColDataSource =  new JRBeanCollectionDataSource(dataList);
+    		jasperPrint = JasperFillManager.fillReport(reportStream, parameters, beanColDataSource);
+    		pdf = JasperExportManager.exportReportToPdf(jasperPrint);
+    		ByteArrayInputStream pdfStream = new ByteArrayInputStream(pdf);
+
+            Response.ResponseBuilder response = Response.ok(pdfStream);
+            response.header("Content-Disposition", "inline; filename=Request Report.pdf");
+            return response.build();
+    	} catch (JRException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+		return null;
+	}
+	
+	public BufferedImage getLogo() {
+		//GET THE LOGO FILE FROM RESOURCE
+		InputStream reportLogo = getClass().getResourceAsStream("/images/report-header-logo.png");
+		
+		 BufferedImage imfg = null;
+		 try {
+		     //InputStream in = new ByteArrayInputStream(requestReportAltamiraimage);
+		     imfg = ImageIO.read(reportLogo);
+		 } catch (Exception e1) {
+		     e1.printStackTrace();
+		 }
+		 return imfg;
+	}
+
+}
--- src/main/java/bpm/manufacturing/bom/report/Reports.java
+++ src/main/java/bpm/manufacturing/bom/report/Reports.java
@@ -1,18 +1,8 @@
 package bpm.manufacturing.bom.report;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
-
-import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,24 +10,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 /**
  * Root resource (exposed at "reports" path)
@@ -45,261 +19,23 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Path("reports")
 public class Reports {
-
-    /**
+	
+	/**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "application/pdf" media type.
-     * @return 
+	 * @return 
      * @return 
      *
      */
 	@GET @Path("/materials/{orderId}")
     @Produces("application/pdf") 
-    public  Response getIt(@Context HttpServletRequest req, @Context HttpServletResponse resp, @PathParam("orderId") String paramOrderId) throws ServletException, IOException {
+    public  Response materialListReport(@Context HttpServletRequest req, @Context HttpServletResponse resp, @PathParam("orderId") String orderId) throws ServletException, IOException {
 		
-		//GET THE LOGO FILE FROM RESOURCE
-		InputStream reportLogo = getClass().getResourceAsStream("/images/report-header-logo.png");
-
-		BufferedImage imfg = null;
-		try {
-			//InputStream in = new ByteArrayInputStream(requestReportAltamiraimage);
-			imfg = ImageIO.read(reportLogo);
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-        
-		//GET THE JASPER FILE
-		InputStream reportStream = getClass().getResourceAsStream("/reports/bom-by-order-report.jasper");
-		if (reportStream == null) {
-        	return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Não foi possivel carregar o relatorio !").build();
-        }
+		MaterialListReport materialListReport = new MaterialListReport();
+		return materialListReport.getReport(orderId);	
 		
-    	ArrayList<BomDataBean> dataList = new ArrayList<BomDataBean>();
-    	Map<String, Object> parameters = new HashMap<String, Object>();
-    	JasperPrint jasperPrint;
-    	byte[] pdf = null;
-		
-		//GET THE REPORT DATA FROM API
-		try {
-			Client client = ClientBuilder.newClient();
-			WebTarget webTarget = client.target("http://altamira-api.elasticbeanstalk.com/webapi/order/");
-			String responseEntity = webTarget.path(paramOrderId).request(MediaType.APPLICATION_JSON).get(String.class);
-			JSONParser parser=new JSONParser();
-			
-			//PARSE JSON DATA- STRING TO JSON OBJECT
-			try {
-				Object responseObj = parser.parse(responseEntity);
-				JSONObject responseJsonObj = (JSONObject)responseObj;
-				
-				JSONObject orderJsonObj = responseJsonObj;
-				long orderNumber = (long) orderJsonObj.get("number");
-				String customer = (String) orderJsonObj.get("customer");
-				String salesRepresentative = (String) orderJsonObj.get("representative");
-				long createdTimeStamp = (long) orderJsonObj.get("created");
-				long deliveryTimeStamp = (long) orderJsonObj.get("delivery");
-				double sumTotalWeight = (double) 0.00;
-				
-				String quotation = (String) orderJsonObj.get("quotation");
-				String comment = (String) orderJsonObj.get("comment");
-				
-				//FORMATING ORDER DATE
-				String orderDateDisplay = new java.text.SimpleDateFormat("dd/MM/yyyy").format(new java.util.Date(createdTimeStamp));
-				
-				//FORMATING DELIVERT DATE
-				String deliveryDateDisplay = new java.text.SimpleDateFormat("w/yyyy").format(new java.util.Date(deliveryTimeStamp));
-				String weekString = new java.text.SimpleDateFormat("w").format(new java.util.Date(deliveryTimeStamp));
-				int weekNo = Integer.parseInt( weekString );
-				String yearString = new java.text.SimpleDateFormat("yyyy").format(new java.util.Date(deliveryTimeStamp));
-				int yearNo = Integer.parseInt( yearString );
-				
-				
+	}
 
-				// CALCULATE THE 1ST DAY AND LAST DAY OF THE WEEK
-				Calendar calendar = Calendar.getInstance();
-				calendar.clear();
-				calendar.set(Calendar.WEEK_OF_YEAR, weekNo);
-				calendar.set(Calendar.YEAR, yearNo);
-				// Get the FIRST day of week.
-				Date date = calendar.getTime();
-				String startDateDisplay = new java.text.SimpleDateFormat("dd/MM/yyyy").format(date);
-				// Get the LAST day of week.
-				calendar.add(Calendar.DATE, 6);
-				Date endDate = calendar.getTime();
-				String endDateDisplay = new java.text.SimpleDateFormat("dd/MM/yyyy").format(endDate);
-				String deliveryWeekDisplay = startDateDisplay + " a " + endDateDisplay;
-				    
-				//SET THE PARAMETERS FOR JASPER REPORT
-		        parameters.put("Title", "Lista de Material - " + orderNumber);
-		     	parameters.put("UserName", "MASTER");
-		     	parameters.put("Customer", customer);
-		     	parameters.put("Representative", salesRepresentative);
-		     	parameters.put("OrderID", orderNumber);
-		     	parameters.put("LogoImage", imfg);
-		     	parameters.put("DateRequest", orderDateDisplay);
-		     	parameters.put("DeliveryTime", deliveryDateDisplay);
-		     	parameters.put("DeliveryWeek", deliveryWeekDisplay);
-		     	parameters.put("NoBudget", quotation);
-		     	// TODO REMOVE THE HARD-CODE VALUE FOR NoProject
-		     	parameters.put("NoProject", (String) orderJsonObj.get("project"));
-		     	parameters.put("Finish", (String) orderJsonObj.get("finish"));
-		     	parameters.put("Comment", comment);
-		     	
-		     	Object itemJsonObject = orderJsonObj.get("item");
-		     	if (itemJsonObject instanceof JSONArray) {
-		     		JSONArray itemJsonArray = (JSONArray)orderJsonObj.get("item");
-		     		for(int i = 0; i < itemJsonArray.size(); i++) {
-						Object itemObject = itemJsonArray.get(i);
-						JSONObject itemJsonObj = (JSONObject)itemObject;
-						long itemNo = (long) itemJsonObj.get("item");
-						String itemDescription = (String) itemJsonObj.get("description");
-						
-						Object prodJsonObject = itemJsonObj.get("product");
-						if (prodJsonObject instanceof JSONArray) {
-							JSONArray prodJsonArray = (JSONArray)itemJsonObj.get("product");
-				            for(int j = 0; j < prodJsonArray.size(); j++) {
-								Object prodObject = prodJsonArray.get(j);
-								JSONObject prodJsonObj = (JSONObject)prodObject;
-								
-								double weightTotal = (double) prodJsonObj.get("weight");
-								String color = (String) prodJsonObj.get("color");
-								String prodDescription = (String) prodJsonObj.get("description");
-								double quantity = (double) prodJsonObj.get("quantity");
-								String prodCode = (String) prodJsonObj.get("code");
-								
-								//ADD TO SUM TOTAL WEIGHT
-								sumTotalWeight = sumTotalWeight + weightTotal;
-	
-								//CREATE THE OBJECT
-								BomDataBean dataBean = new BomDataBean();
-								dataBean.setItemNo(itemNo);
-								dataBean.setItemDescription(itemDescription);
-								dataBean.setProdQty(quantity);
-								dataBean.setProdDescription(prodDescription);
-								dataBean.setProdColor(color);
-								dataBean.setProdWeight(weightTotal/quantity);
-								dataBean.setProdWeightTotal(weightTotal);
-	
-								//ADD THE OBJECT TO DATALIST
-								dataList.add(dataBean);		               
-				            }
-						}
-						else {
-							Object prodObject = prodJsonObject;
-							JSONObject prodJsonObj = (JSONObject)prodObject;
-							
-							double weightTotal = (double) prodJsonObj.get("weight");
-							String color = (String) prodJsonObj.get("color");
-							String prodDescription = (String) prodJsonObj.get("description");
-							double quantity = (double) prodJsonObj.get("quantity");
-							String prodCode = (String) prodJsonObj.get("code");
-							
-							//ADD TO SUM TOTAL WEIGHT
-							sumTotalWeight = sumTotalWeight + weightTotal;
-
-							//CREATE THE OBJECT
-							BomDataBean dataBean = new BomDataBean();
-							dataBean.setItemNo(itemNo);
-							dataBean.setItemDescription(itemDescription);
-							dataBean.setProdQty(quantity);
-							dataBean.setProdDescription(prodDescription);
-							dataBean.setProdColor(color);
-							dataBean.setProdWeight(weightTotal/quantity);
-							dataBean.setProdWeightTotal(weightTotal);
-
-							//ADD THE OBJECT TO DATALIST
-							dataList.add(dataBean);		
-						}
-			        }
-		     	}
-		     	else {
-		     		Object itemObject = itemJsonObject;
-					JSONObject itemJsonObj = (JSONObject)itemObject;
-					long itemNo = (long) itemJsonObj.get("item");
-					String itemDescription = (String) itemJsonObj.get("description");
-					Object prodJsonObject = itemJsonObj.get("product");
-					if (prodJsonObject instanceof JSONArray) {
-						JSONArray prodJsonArray = (JSONArray)itemJsonObj.get("product");
-			            for(int j = 0; j < prodJsonArray.size(); j++) {
-							Object prodObject = prodJsonArray.get(j);
-							JSONObject prodJsonObj = (JSONObject)prodObject;
-							
-							double weightTotal = (double) prodJsonObj.get("weight");
-							String color = (String) prodJsonObj.get("color");
-							String prodDescription = (String) prodJsonObj.get("description");
-							double quantity = (double) prodJsonObj.get("quantity");
-							String prodCode = (String) prodJsonObj.get("code");
-							
-							//ADD TO SUM TOTAL WEIGHT
-							sumTotalWeight = sumTotalWeight + weightTotal;
-
-							//CREATE THE OBJECT
-							BomDataBean dataBean = new BomDataBean();
-							dataBean.setItemNo(itemNo);
-							dataBean.setItemDescription(itemDescription);
-							dataBean.setProdQty(quantity);
-							dataBean.setProdDescription(prodDescription);
-							dataBean.setProdColor(color);
-							dataBean.setProdWeight(weightTotal/quantity);
-							dataBean.setProdWeightTotal(weightTotal);
-
-							//ADD THE OBJECT TO DATALIST
-							dataList.add(dataBean);		               
-			            }
-					}
-					else {
-						Object prodObject = prodJsonObject;
-						JSONObject prodJsonObj = (JSONObject)prodObject;
-						
-						double weightTotal = (double) prodJsonObj.get("weight");
-						String color = (String) prodJsonObj.get("color");
-						String prodDescription = (String) prodJsonObj.get("description");
-						double quantity = (double) prodJsonObj.get("quantity");
-						String prodCode = (String) prodJsonObj.get("code");
-						
-						//ADD TO SUM TOTAL WEIGHT
-						sumTotalWeight = sumTotalWeight + weightTotal;
-
-						//CREATE THE OBJECT
-						BomDataBean dataBean = new BomDataBean();
-						dataBean.setItemNo(itemNo);
-						dataBean.setItemDescription(itemDescription);
-						dataBean.setProdQty(quantity);
-						dataBean.setProdDescription(prodDescription);
-						dataBean.setProdColor(color);
-						dataBean.setProdWeight(weightTotal/quantity);
-						dataBean.setProdWeightTotal(weightTotal);
-
-						//ADD THE OBJECT TO DATALIST
-						dataList.add(dataBean);		
-					}
-		        }
-		     	parameters.put("SumTotalWeight", sumTotalWeight);
-			} catch(ParseException pe) {
-				System.out.println("position: " + pe.getPosition());
-				System.out.println(pe);
-			}
-
-		} catch (Exception e) {
-				e.printStackTrace();
-				return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-		}
-		
-		//PRINT THE PDF REPORT
-    	try {
-    		JRBeanCollectionDataSource beanColDataSource =  new JRBeanCollectionDataSource(dataList);
-    		jasperPrint = JasperFillManager.fillReport(reportStream, parameters, beanColDataSource);
-    		pdf = JasperExportManager.exportReportToPdf(jasperPrint);
-    		ByteArrayInputStream pdfStream = new ByteArrayInputStream(pdf);
-
-            Response.ResponseBuilder response = Response.ok(pdfStream);
-            response.header("Content-Disposition", "inline; filename=Request Report.pdf");
-            return response.build();
-    	} catch (JRException e) {
-    		// TODO Auto-generated catch block
-    		e.printStackTrace();
-    	}
-		return null;
-    }
 	
 	/**
      * Method handling HTTP GET requests. The returned object will be sent
